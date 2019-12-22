5794500e35d9f7db03169f8de9ad2ef360b29df5
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-22 20:40:50 +0100

    Simple Dockerfile example

---
+++ Dockerfile/simple/someCommands.bat
@@ -0,0 +1,9 @@
+@echo  off
+echo.
+echo.  This text is printed by someCommands.bat
+echo.
+echo.  Value of TQ84_VAR is "%TQ84_VAR%"
+echo.
+echo.  Current directory is %CD%
+echo.  Files in current directory are
+dir /s /b
